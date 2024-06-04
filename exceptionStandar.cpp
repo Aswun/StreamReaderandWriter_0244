09ce4b632c23379e0a4d81f6e8bbe22acc05e658
Aswun <aswinlutfian32@gmail.com>
2024-06-04 15:05:54 +0700

    menambahkan int main

--- exceptionStandar.cpp
+++ exceptionStandar.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
-#include <exception> // untuk obyek exception yang akan digunakan
-#include <array> // untuk obyek array yang akan kita gunakan
+#include <exception>  // untuk obyek exception yang akan digunakan
+#include <array>  // untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl;  // penanda 1
+}
